54da58efa9d61475b1fbae57759fa323e72547d7
FRNDZNE <hafizptr_14@yahoo.com>
2023-10-16 17:40:45 +0700

    migration table sementara

---
+++ database/migrations/2023_10_16_092716_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('angkatan_id')->references('id')->on('angkatans')->onDelete('cascade');
+            $table->foreignId('kelas_id')->references('id')->on('kelas')->onDelete('cascade');
+            $table->foreignId('semester_id')->references('id')->on('semesters')->onDelete('cascade');
+            $table->string('nim');
+            $table->string('name');
+            $table->enum('gender',['pria','wanita']);
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
--- database/migrations/2023_10_16_100435_create_dosens_table.php
+++ database/migrations/2023_10_16_100435_create_dosens_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('dosens', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('nip');
+            $table->string('name');
+            $table->enum('gender',['pria','wanita']);
+            $table->string('foto');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('dosens');
     }
 };
