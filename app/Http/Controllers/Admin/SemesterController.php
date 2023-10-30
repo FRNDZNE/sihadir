c349d9c8192a551f759e43c3548dc27574e354c0
Fitri Ambarwati <fitriamw3@gmail.com>
2023-10-30 22:36:59 +0700

    crud semester

---
+++ app/Http/Controllers/Admin/SemesterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Semester;
+
+class SemesterController extends Controller
+{
+    public function index()
+    {
+        $semester = Semester::all();
+        return view('admin.data.semester', compact('semester'));
+
+    }
+    public function store(Request $request)
+    {
+        Semester::create([
+            'name' => $request->name,
+        ]);
+        return redirect()->back();
+    }
+    public function update(Request $request){
+        $data = Semester::find( $request->id );
+        $data->name = $request->name;
+        $data->save();
+        return redirect()->back();
+    }
+    
+    public function delete($id)
+    {
+        Semester::find($id)->delete();
+        return redirect()->back();
+    }
+}
