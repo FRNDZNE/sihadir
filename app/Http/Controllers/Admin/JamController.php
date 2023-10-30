49b69bac45003a6d75dddc60aa61b3e2848fb4b4
Farozi07 <zideansaf320@gmail.com>
2023-10-30 22:03:01 +0700

    crud jam admin

---
+++ app/Http/Controllers/Admin/JamController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Jam;
+
+class JamController extends Controller
+{
+    public function index(){
+        // cek data table jam
+        $jam = Jam::all();
+        // return $data;
+        return view ('admin.data.jam',compact('jam'));
+
+    }
+    public function store(Request $request){
+        $data = new Jam;
+        $data->name = $request->name;
+        $data->save();
+        return redirect()->back();
+
+    }
+    public function update(Request $request){
+        // $data= Jam::find($request->id);
+        $data= Jam::where('id',$request->id)->first();
+
+        $data->update([
+            'name'=>$request->name,
+        ]);
+        return redirect()->back();
+    }
+    public function delete($id){
+        Jam::find($id)->delete();
+        return redirect()->back();
+    }
+
+}
--- app/Models/Jam.php
+++ app/Models/Jam.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Jam extends Model
 {
     use HasFactory;
+    protected $table ='jams';
+    protected $guarded =[];
 }
