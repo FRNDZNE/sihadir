590c812e7cdba021b1951d4344686632625efea4
Farozi07 <zideansaf320@gmail.com>
2023-10-30 16:22:20 +0700

    routing crud admin untuk table mahasiswa

--- routes/admin.php
+++ routes/admin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     Route::middleware(['auth','role:admin'])->prefix('admin')->group(function(){
         // Show Dashboard
         Route::get('/dashboard',[App\Http\Controllers\DashboardController::class,'admin'])->name('admin.dashboard');
@@ -12,5 +12,10 @@
         Route::post('/dosen/delete/{id}',[App\Http\Controllers\Admin\DosenController::class,'delete'])->name('admin.dosen.delete');
 
         // Route CRUD MAHASISWA
-        
+        Route::get('/mahasiswa',[App\Http\Controllers\Admin\MahasiswaController::class,'index'])->name('admin.mahasiswa.index');
+        Route::get('/mahasiswa/create',[App\Http\Controllers\Admin\MahasiswaController::class,'create'])->name('admin.mahasiswa.create');
+        Route::post('/mahasiswa/store',[App\Http\Controllers\Admin\MahasiswaController::class,'store'])->name('admin.mahasiswa.store');
+        Route::get('/mahasiswa/edit/{id}',[App\Http\Controllers\Admin\MahasiswaController::class,'edit'])->name('admin.mahasiswa.edit');
+        Route::post('/mahasiswa/update/{id}',[App\Http\Controllers\Admin\MahasiswaController::class,'update'])->name('admin.mahasiswa.update');
+        Route::post('/mahasiswa/delete/{id}',[App\Http\Controllers\Admin\MahasiswaController::class,'delete'])->name('admin.mahasiswa.delete');
     });
