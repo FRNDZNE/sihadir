d1c02e9c1f1d72fc28b93dbaab79794cccb9b415
FRNDZNE <hafizptr_14@yahoo.com>
2023-10-30 10:03:14 +0700

    create dosen

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Dosen extends Model
 {
     use HasFactory;
+    protected $table = 'dosens';
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -2,4 +2,15 @@
     Route::middleware(['auth','role:admin'])->prefix('admin')->group(function(){
         // Show Dashboard
         Route::get('/dashboard',[App\Http\Controllers\DashboardController::class,'admin'])->name('admin.dashboard');
+
+        // Route CRUD DOSEN
+        Route::get('/dosen',[App\Http\Controllers\Admin\DosenController::class,'index'])->name('admin.dosen.index');
+        Route::get('/dosen/create',[App\Http\Controllers\Admin\DosenController::class,'create'])->name('admin.dosen.create');
+        Route::post('/dosen/store',[App\Http\Controllers\Admin\DosenController::class,'store'])->name('admin.dosen.store');
+        Route::get('/dosen/edit/{id}',[App\Http\Controllers\Admin\DosenController::class,'edit'])->name('admin.dosen.edit');
+        Route::post('/dosen/update/{id}',[App\Http\Controllers\Admin\DosenController::class,'update'])->name('admin.dosen.update');
+        Route::post('/dosen/delete/{id}',[App\Http\Controllers\Admin\DosenController::class,'delete'])->name('admin.dosen.delete');
+
+        // Route CRUD MAHASISWA
+        
     });
