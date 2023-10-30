eb3672b63edbe8ff131cf58d6a2fdcd1fcde2e03
KrismaKrisma <krismaa304@gmail.com>
2023-10-30 22:12:30 +0700

    CRUD  KELAS

--- resources/views/admin/data/kelas.blade.php
+++ resources/views/admin/data/kelas.blade.php
@@ -52,71 +52,63 @@
                         <td>
                             <!-- Button trigger modal -->
                             <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#modaledit-{{$k->id}}">
-                            <i class="fas fa-edit"></i> 
+                                <i class="fas fa-edit"></i> 
                             </button>
                             <!-- Modal -->
                             <div class="modal fade" id="modaledit-{{$k->id}}" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
-                                        <div class="modal-header bg-primary">
+                                        <div class="modal-header bg-warning">
                                             <h5 class="modal-title"> Tambah Kelas</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
-                                    <form action="{{route('admin.kelas.update')}}" method="post">
-                                        @csrf
-                                        <div class="modal-body">
-                                            <input type="hidden" name="id" value="{{$k->id}}">
-                                            <div class="form-group">
-                                                <label for="kelas">Kelas</label>
-                                                <input type="text" name="name" id="kelas" value="{{$k->name}}" class="form-control" placeholder="Masukan Kelas" aria-describedby="helpId">
-                                            </div>
+                                        <form action="{{route('admin.kelas.update')}}" method="post">
+                                            @csrf
+                                            <div class="modal-body">
+                                                <input type="hidden" name="id" value="{{$k->id}}">
+                                                <div class="form-group">
+                                                    <label for="kelas">Kelas</label>
+                                                    <input type="text" name="name" id="kelas" value="{{$k->name}}" class="form-control" placeholder="Masukan Kelas" aria-describedby="helpId">
+                                                </div>
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                                <button type="submit" class="btn btn-warning">Simpan</button>
                                             </div>
-                                        </div>
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>  
                             </div>
-                            
                             <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modalhapus-{{$k->id}}">
-                            <i class="fas fa-trash"></i>
+                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modaldelete-{{$k->id}}">
+                              <i class="fas fa-trash"></i>
                             </button>
-
-                            <!-- Modal -->
-                            <div class="modal fade" id="modalhapus-{{$k->id}}" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                <div class="modal-header bg-primary">
-                                    <h5 class="modal-title"> Tambah Kelas</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <form action="{{route('admin.kelas.update')}}" method="post">
-                                <div class="modal-body">
-                                    Yakin ingin menghapus {{$k->name}}
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <form action="{{ route('admin.kelas.delete',$k->id) }}" method="post">
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger">Hapus</button>
-                                    </form>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-
-
                             
-
-
+                            <!-- Modal -->
+                            <div class="modal fade" id="modaldelete-{{$k->id}}" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header bg-danger">
+                                            <h5 class="modal-title">Hapus Kelas</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Ingin menghapus kelas {{$k->name}} ?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <form action="{{route('admin.kelas.delete',$k->id)}}" method="post">
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                            
                         </td>
                     </tr>
                     @endforeach
